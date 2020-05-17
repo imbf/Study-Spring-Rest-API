407cbe6c0e1b4d11324fe6fea8f71652de207b8c
배종진 <qowhdwlsdk@gmail.com>
2020-05-17 23:26:59 +0900

    Feat: free와 offline 상태를 update할 수 있는 메소드 구현

--- src/main/java/com/greenfrog/restapi/events/Event.java
+++ src/main/java/com/greenfrog/restapi/events/Event.java
@@ -29,4 +29,19 @@ public class Event {
     private EventStatus eventStatus = EventStatus.DRAFT;
 
 
+    public void update() {
+        // Update Free
+        if ( this.basePrice == 0 && this.maxPrice == 0) {
+            this.free = true;
+        } else  {
+            this.free = false;
+        }
+
+        // Update offline
+        if (this.location == null && this.location.isBlank()) {
+            this.offline = false;
+        } else {
+            this.offline = true;
+        }
+    }
 }
--- src/main/java/com/greenfrog/restapi/events/EventController.java
+++ src/main/java/com/greenfrog/restapi/events/EventController.java
@@ -40,6 +40,7 @@ public class EventController {
         }
 
         Event event = modelMapper.map(eventDTO, Event.class);
+        event.update();
         Event savedEvent = this.eventRepository.save(event);
         URI createdUri = linkTo(EventController.class).slash(savedEvent.getId()).toUri();
         return ResponseEntity.created(createdUri).body(event);
