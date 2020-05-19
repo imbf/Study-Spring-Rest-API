5580408b18b4ee8699e3040b407b46720ba3d64b
배종진 <qowhdwlsdk@gmail.com>
2020-05-19 21:39:30 +0900

    Test: POST /api/events 요청시 에러발생에 따른 HATEOAS 적용 응답 테스트

--- src/test/java/com/greenfrog/restapi/events/EventControllerTests.java
+++ src/test/java/com/greenfrog/restapi/events/EventControllerTests.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -33,6 +34,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 @Import(RestDocsConfiguration.class)
+@ActiveProfiles("test")
 public class EventControllerTests {
 
     @Autowired
@@ -188,9 +190,10 @@ public class EventControllerTests {
                     .content(objectMapper.writeValueAsString(eventDTO)))
                 .andDo(print())
                 .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$[0].objectName").exists())
-                .andExpect(jsonPath("$[0].defaultMessage").exists())
-                .andExpect(jsonPath("$[0].code").exists())
+                .andExpect(jsonPath("content[0].objectName").exists())
+                .andExpect(jsonPath("content[0].defaultMessage").exists())
+                .andExpect(jsonPath("content[0].code").exists())
+                .andExpect(jsonPath("_links.index").exists())
         ;
 
     }
