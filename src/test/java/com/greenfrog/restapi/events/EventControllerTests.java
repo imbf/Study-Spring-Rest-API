dbf728edb1ead49f4641e5d812b21deaeb8d0fec
배종진 <qowhdwlsdk@gmail.com>
2020-05-19 18:14:21 +0900

    Test: /api/events profile link Test

--- src/test/java/com/greenfrog/restapi/events/EventControllerTests.java
+++ src/test/java/com/greenfrog/restapi/events/EventControllerTests.java
@@ -76,7 +76,8 @@ public class EventControllerTests {
                         links(
                                 linkWithRel("self").description("link to self"),
                                 linkWithRel("query-events").description("link to query events"),
-                                linkWithRel("update-event").description("link to update an existing")
+                                linkWithRel("update-event").description("link to update an existing"),
+                                linkWithRel("profile").description("link to profile")
                         ),
                         requestHeaders(
                                 headerWithName(HttpHeaders.ACCEPT).description("accept header"),
@@ -114,7 +115,8 @@ public class EventControllerTests {
                                 fieldWithPath("eventStatus").description("event Status"),
                                 fieldWithPath("_links.self.href").description("event status"),
                                 fieldWithPath("_links.query-events.href").description("link to query event list"),
-                                fieldWithPath("_links.update-event.href").description("link to update existing event")
+                                fieldWithPath("_links.update-event.href").description("link to update existing event"),
+                                fieldWithPath("_links.profile").description("link to profile")
                         )
 
 
