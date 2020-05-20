8ea6f39c5de06a86e447e94b92c6a72f821487b1
배종진 <qowhdwlsdk@gmail.com>
2020-05-20 10:55:02 +0900

    test: GET /api/events/{id} 테스트

--- src/test/java/com/greenfrog/restapi/events/EventControllerTests.java
+++ src/test/java/com/greenfrog/restapi/events/EventControllerTests.java
@@ -199,7 +199,6 @@ public class EventControllerTests {
                 .andExpect(jsonPath("content[0].objectName").exists())
                 .andExpect(jsonPath("content[0].defaultMessage").exists())
                 .andExpect(jsonPath("content[0].code").exists())
-                .andExpect(jsonPath("_links.index").exists())
         ;
 
     }
@@ -210,7 +209,7 @@ public class EventControllerTests {
         // Given
         IntStream.range(0, 30).forEach(this::generateEvent);
 
-        // When
+        // When & Then
         this.mockMvc.perform(get("/api/events")
                     .param("page","1")
                     .param("size","10")
@@ -224,17 +223,42 @@ public class EventControllerTests {
                 .andExpect(jsonPath("_links.profile").exists())
                 .andDo(document("query-events"))
         ;
-
-
-        // Then
     }
 
-    private void generateEvent(int index) {
+    private Event generateEvent(int index) {
         Event event = Event.builder()
                 .name("event" + index)
                 .description("test event")
                 .build();
 
-        this.eventRepository.save(event);
+        return this.eventRepository.save(event);
     }
+
+    @Test
+    @TestDescription("기존의 이벤트를 하나 조회하기")
+    public void getEvent() throws Exception {
+        // Given
+        Event event = this.generateEvent(100);
+
+        // When & Then
+        this.mockMvc.perform(get("/api/events/{id}", event.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("name").exists())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(jsonPath("_links.self").exists())
+                .andExpect(jsonPath("_links.profile").exists())
+                .andDo(document("get-an-event"))
+        ;
+    }
+
+    @Test
+    @TestDescription("없는 이벤트를 조회했을 때 404 응답 받기")
+    public void getEvent404() throws  Exception {
+        // When & Then
+        this.mockMvc.perform(get("/api/events/123123"))
+                .andExpect(status().isNotFound())
+
+        ;
+    }
+
 }
