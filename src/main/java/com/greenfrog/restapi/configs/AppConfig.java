ea14f4df14ad65208ca11d047433ea151ca9c643
배종진 <qowhdwlsdk@gmail.com>
2020-05-22 23:49:13 +0900

    fix: Config내에  ApplicationRunner 메소드 삭제

--- src/main/java/com/greenfrog/restapi/configs/AppConfig.java
+++ src/main/java/com/greenfrog/restapi/configs/AppConfig.java
@@ -1,18 +1,10 @@
 package com.greenfrog.restapi.configs;
 
-import com.greenfrog.restapi.accounts.Account;
-import com.greenfrog.restapi.accounts.AccountRole;
-import com.greenfrog.restapi.accounts.AccountService;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.Set;
 
 @Configuration
 public class AppConfig {
@@ -27,21 +19,4 @@ public class AppConfig {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-    @Bean
-    public ApplicationRunner applicationRunner() {
-        return new ApplicationRunner() {
-            @Autowired
-            AccountService accountService;
-
-            @Override
-            public void run(ApplicationArguments args) throws Exception {
-                Account keesun = Account.builder()
-                        .email("keesun@email.com")
-                        .password("keesun")
-                        .roles(Set.of(AccountRole.ADMIN,AccountRole.USER))
-                        .build();
-                accountService.saveAccount(keesun);
-            }
-        };
-    }
 }
