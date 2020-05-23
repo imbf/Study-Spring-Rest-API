dc1b84c3c307406104b0fc2fc9527bb82d08393d
배종진 <qowhdwlsdk@gmail.com>
2020-05-23 16:47:49 +0900

    refactor: 컨트롤러 테스트에 인증 및 권한 테스트 리팩토링

--- src/test/java/com/greenfrog/restapi/events/EventControllerTests.java
+++ src/test/java/com/greenfrog/restapi/events/EventControllerTests.java
@@ -134,11 +134,14 @@ public class EventControllerTests extends BaseControllerTest {
         ;
 
     }
-
     private String getBearerToken() throws Exception {
+        return "Bearer" + getAccessToken();
+    }
+
+    private String getAccessToken() throws Exception {
         // Given
-        String username = "keesun@email.com";
-        String password = "keesun";
+        String username = "user@email.com";
+        String password = "user";
         Account keesun = Account.builder()
                 .email(username)
                 .password(password)
@@ -158,6 +161,29 @@ public class EventControllerTests extends BaseControllerTest {
         String result = perform.andReturn().getResponse().getContentAsString();
         Jackson2JsonParser parser = new Jackson2JsonParser();
         return parser.parseMap(result).get("access_token").toString();
+    }
+
+    @Test
+    @TestDescription("30개의 이벤트를 10개씩 두번째 페이지 조회하기")
+    public void queryEventsWithAuthentication() throws Exception {
+        // Given
+        IntStream.range(0, 30).forEach(this::generateEvent);
+
+        // When & Then
+        this.mockMvc.perform(get("/api/events")
+                .header(HttpHeaders.AUTHORIZATION, getBearerToken())
+                .param("page", "1")
+                .param("size", "10")
+                .param("sort", "name,DESC"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("page").exists())
+                .andExpect(jsonPath("_embedded.eventList[0]._links.self").exists())
+                .andExpect(jsonPath("_links.self").exists())
+                .andExpect(jsonPath("_links.profile").exists())
+//                .andExpect(jsonPath("_links.create-event").exists())
+                .andDo(document("query-events"))
+        ;
     }
 
     @Test
